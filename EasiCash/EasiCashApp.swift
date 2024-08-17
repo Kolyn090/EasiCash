3a1406762feb7354300ab983c3bc36e5bf09a2d1
Yongye Tan <yongyetan1209@gmail.com>
2024-08-16 22:45:11 -0400

    First Commit

---
+++ EasiCash/EasiCashApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasiCashApp.swift
+//  EasiCash
+//
+//  Created by Yongye on 8/16/24.
+//
+
+import SwiftUI
+
+@main
+struct EasiCashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EasiCashTests/EasiCashTests.swift
@@ -0,0 +1,16 @@
+//
+//  EasiCashTests.swift
+//  EasiCashTests
+//
+//  Created by Yongye on 8/16/24.
+//
+
+import Testing
+
+struct EasiCashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
