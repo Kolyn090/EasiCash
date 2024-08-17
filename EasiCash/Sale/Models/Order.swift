e599813382f3e8bf5cff616f94cea087f5817054
Chengtao <chengtaolinctl@gmail.com>
2024-08-17 11:34:24 -0400

    Updated: SaleView Table

---
+++ EasiCash/Sale/Models/Order.swift
@@ -0,0 +1,22 @@
+//
+//  Order.swift
+//  EasiCash
+//
+//  Created by CHENGTAO on 8/17/24.
+//
+
+import Foundation
+
+enum OrderType: String, CaseIterable {
+    case online = "online"
+    case inStore = "inStore"
+}
+
+struct Order: Identifiable {
+    let id: UUID
+    let price: Double
+    let items: [Item]
+    let createdAt: Date
+    let type: OrderType
+    
+}
--- EasiCash/Sale/Views/SaleTabView.swift
+++ EasiCash/Sale/Views/SaleTabView.swift
@@ -8,7 +8,45 @@
 import SwiftUI
 
 struct SaleTabView: View {
+    @State private var orders: [Order] = [
+        Order(id: UUID(), price: 49.99, items: [Item(title: "Item 1", price: 19.99, quantity: 1)], createdAt: Date(), type: .online),
+        Order(id: UUID(), price: 79.99, items: [Item(title: "Item 2", price: 79.99, quantity: 1)], createdAt: Date(), type: .inStore)
+    ]
+    
     var body: some View {
-        Text("sale")
+        VStack {
+            Table(orders) {
+                TableColumn("Order ID") { order in
+                    Text(order.id.uuidString.prefix(16))
+                }
+                
+                TableColumn("Price") { order in
+                    Text(String(format: "$%.2f", order.price))
+                }
+                
+                TableColumn("Order Type") { order in
+                    Text(order.type.rawValue.capitalized)
+                }
+                
+                TableColumn("Created At") { order in
+                    Text(order.createdAt, formatter: dateFormatter)
+                }
+            }
+            .frame(minWidth: 600, minHeight: 400) // Adjust frame as needed
+        }
+    }
+    
+    // Date Formatter for displaying date
+    private var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .short
+        return formatter
+    }
+}
+
+struct SaleTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        SaleTabView()
     }
 }
