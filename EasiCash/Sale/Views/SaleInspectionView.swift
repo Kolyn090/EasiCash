f7072f0328c71f902fe801c60c8a0dc003229c26
Chengtao <chengtaolinctl@gmail.com>
2024-08-17 15:56:16 -0400

    added: Item inspector view

---
+++ EasiCash/Sale/Views/SaleInspectionView.swift
@@ -0,0 +1,25 @@
+//
+//  SaleInspectionView.swift
+//  EasiCash
+//
+//  Created by CHENGTAO on 8/17/24.
+//
+
+import SwiftUI
+
+struct SaleInspectionView: View {
+    
+    var order: Order
+    
+    var body: some View {
+        VStack {
+            Text("hi")
+        }
+    }
+}
+//
+//struct SaleInspectionView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SaleInspectionView(orderItems: [])
+//    }
+//}
--- EasiCash/Sale/Views/SaleTabView.swift
+++ EasiCash/Sale/Views/SaleTabView.swift
@@ -6,20 +6,27 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct SaleTabView: View {
     @State private var orders: [Order] = [
-        Order(id: UUID(), price: 49.99, items: [Item(title: "Item 1", price: 19.99, quantity: 1)], createdAt: Date(), type: .online),
-        Order(id: UUID(), price: 79.99, items: [Item(title: "Item 2", price: 79.99, quantity: 1)], createdAt: Date(), type: .inStore)
+        Order(id: UUID(), user: "Hugo", price: 49.99, items: [Item(title: "Item 1", price: 19.99, quantity: 1)], createdAt: Date(), type: .online),
+        Order(id: UUID(), user: "Alice", price: 79.99, items: [Item(title: "Item 2", price: 79.99, quantity: 1)], createdAt: Date(), type: .inStore)
     ]
     
     @State private var isInspectorPresented: Bool = false
     
-    @State private var selectedOrder: Order.ID? = nil
+    @State private var selectedOrderID: Order.ID? = nil
+    
+    private var selectedOrder: Order? {
+        guard let selectedOrderID else { return nil }
+        
+        return orders.filter { $0.id == selectedOrderID }[0]
+    }
 
     var body: some View {
         NavigationStack {
-                Table(orders, selection: $selectedOrder) {
+                Table(orders, selection: $selectedOrderID) {
                     TableColumn("Order ID") { order in
                         Text(order.id.uuidString.prefix(16))
                     }
@@ -42,14 +49,16 @@ struct SaleTabView: View {
                     Button(action: presentItemInspector) {
                         Image(systemName: "info.bubble.fill")
                     }
+                    .disabled(selectedOrderID == nil)
                 }
             }
             .inspector(isPresented: $isInspectorPresented) {
                 // TODO: View to show items
                 if let selectedOrder {
-                    Text("\(selectedOrder)")
+                    SaleInspectionView(order: selectedOrder)
                 }
-            }
+                }
+            
         }
     }
     
