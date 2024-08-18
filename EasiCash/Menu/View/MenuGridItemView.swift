6cd542b5f284284b3fc0ed300646d6a31b5079cb
Yongye Tan <yongyetan1209@gmail.com>
2024-08-17 21:13:13 -0400

    Added number of tapped items on list item view

--- EasiCash/Menu/View/MenuGridItemView.swift
+++ EasiCash/Menu/View/MenuGridItemView.swift
@@ -11,8 +11,14 @@ import DeveloperToolsSupport
 struct MenuGridItemView: View {
     
     @Environment(MenuViewModel.self) var menuViewModel: MenuViewModel
-    
+        
     var item: MenuItem
+    
+    private var quantity: Int {
+        let filteredItems = menuViewModel.customerSelectedItems.items.filter { item.id == $0.id }
+        if filteredItems.isEmpty { return 0 }
+        return filteredItems[0].quantity
+    }
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -29,18 +35,18 @@ struct MenuGridItemView: View {
             }
         }
         .padding(.horizontal, 10)
-//        .overlay(alignment: .topTrailing) {
-//            if item.quantity > 0 {
-//                Circle()
-//                    .fill(Color.white)
-//                    .frame(width: 25)
-//                    .padding(.horizontal, 15)
-//                    .padding(.vertical, 8)
-//                    .overlay {
-//                        Text("\(item.quantity)")
-//                    }
-//            }
-//        }
+        .overlay(alignment: .topTrailing) {
+            if quantity > 0 {
+                Circle()
+                    .fill(Color.white)
+                    .frame(width: 25)
+                    .padding(.horizontal, 15)
+                    .padding(.vertical, 8)
+                    .overlay {
+                        Text("\(quantity)")
+                    }
+            }
+        }
         .onTapGesture {
             withAnimation {
                 menuViewModel.addOrder(with: item)
